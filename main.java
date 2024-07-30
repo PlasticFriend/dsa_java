ec9033355e25e9db1f1bcd8f1c029e74dbb30fb6
PlasticFriend <sayan02.official@gmail.com>
2024-07-30 20:08:15 +0530

    lec 1 finished

--- main.java
+++ main.java
@@ -1,7 +1,8 @@
 public class main {
     public static void main(String[] args){
 
-        System.out.println(7);
+        System.out.println("i am starting java language");
+//        return True;
 
     }
 }
